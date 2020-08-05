d7213c99ba144d660fe86c50f6ed0967bc3bbcfc
Owen Klan <owen.j.klan@gmail.com>
2020-08-05 18:40:12 +1000

    msgd: Add possibly resolved hostname and "no DNS" flag
    If '--no-dns' isn't specified, then we'll try a reverse
    lookup on the IP of the sender of a message. --no-dns will
    result in the unresolved IPv4 address being used at all
    points

--- handlers/alert.py
+++ handlers/alert.py
@@ -11,14 +11,16 @@ def motd():
             "w00t!")
 
 
-def handle_message(message, msglog=None):
+def handle_message(message, msglog=None, hostname=None):
     msg = "\033[31;1m !! ALERT !!  " + message.message + "\033[0m"
     logging.info(msg)
     if msglog is not None:
         msglog.write(msg)
         msglog.flush()
     icon = GdkPixbuf.Pixbuf.new_from_file("icons/alert.png")
-    notification = Notify.Notification.new(message.from_ip, message.message)
+    notification = Notify.Notification.new(
+        hostname if hostname is not None else message.from_ip,
+        message.message)
     notification.set_icon_from_pixbuf(icon)
     notification.set_image_from_pixbuf(icon)
     notification.set_urgency(2)
