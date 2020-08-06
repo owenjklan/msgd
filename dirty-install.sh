8139da4060a67483b6291b9f3e39b49e0881a8f3
Owen Klan <owen.j.klan@gmail.com>
2020-08-06 19:21:38 +1000

    msgd: Make 'supported' type handler a builtin
    The idea here is that a client could send a 'supported'
    message type. The intention being that the server actually
    sends a response to the client, which is waiting patiently
    for the response.

---
+++ dirty-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Dirty installer - to get you running quickly
+#
+# The intention of this script is for a quick and
+# dirty use of msgd, from cloning the git repo
+# to running the service or using the client.
